1cb623b3d0577b8e5c5bf0b13de69f64cfa10b9c
annabelxu <annabel.xu@chicagobooth.edu>
2022-05-05 15:47:03 -0500

    v1

--- analytics/main.py
+++ analytics/main.py
@@ -1,11 +1,18 @@
 from flask import Flask, redirect, request, jsonify, render_template
+from google.cloud import storage
+from google.cloud import datastore
 
 app = Flask(__name__)
 
 @app.route("/analytics/")
 def homepage():
-  return "hello from analytics", 200
-    
+    datastore_client = datastore.Client.from_service_account_json('photo-timeline-shared-new-90ee5b0c603d.json')
+
+    query = datastore_client.query(kind='Photos')
+    image_entities = list(query.fetch())
+
+    return render_template('homepage.html', image_entities=image_entities)
+
 
 if __name__ == '__main__':
   app.run(debug=True)
